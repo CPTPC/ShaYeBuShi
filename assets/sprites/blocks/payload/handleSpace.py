5456985efcf1c5c19f273361b1b0b9d3c794146a
CPTPC <141936872+CPTPC@users.noreply.github.com>
2024-09-10 19:03:37 +0800

    Add files via upload

---
+++ assets/sprites/blocks/payload/handleSpace.py
@@ -0,0 +1,10 @@
+from PIL import Image as im 
+I = im.open("daxingzaihechuansongdai-top.png")
+I = I.convert("RGBA")
+II = I.load()
+for i in range(I.size[0]):
+    for x in range(I.size[1]):
+        III,IV,V,VI = II[i,x]
+        if (III >= 200 and IV >= 200 and V >= 200):
+            II[i,x] = (III,IV,V,0)
+I.save("daxingzaihechuansongdai-top.png")
