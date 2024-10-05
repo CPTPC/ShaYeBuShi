db3867961a140ba306cabc2f91d6de469d1f928d
CPTPC <141936872+CPTPC@users.noreply.github.com>
2024-10-05 21:10:49 +0800

    Delete SYBSBuildVisibility.java

--- SYBSBuildVisibility.java
+++
@@ -1,8 +0,0 @@
-package shayebushi;
-
-import mindustry.core.Version;
-import mindustry.world.meta.BuildVisibility;
-
-public class SYBSBuildVisibility {
-    public static BuildVisibility tiaoshiOnly = new BuildVisibility(() -> Version.build == -1 || ShaYeBuShi.tiaoshi) ;
-}
